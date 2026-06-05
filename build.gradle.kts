7845a10344a2338d2c4340eee5c631f5f69f09df
Xinlun Zhao <xinlunzhao0326@gmail.com>
2026-06-05 15:37:55 +0800

    plain Gradle -> Spring Boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "4.0.6"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.zhaoxinlun"
@@ -9,9 +11,16 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
---
+++ src/main/java/org/zhaoxinlun/PlantSimulationTestbedApplication.java
@@ -0,0 +1,12 @@
+package org.zhaoxinlun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantSimulationTestbedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantSimulationTestbedApplication.class, args);
+    }
+}
